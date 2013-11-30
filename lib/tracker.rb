cdf200261a3cb0fd1998259ff5f5fe3183e7acb0
ericz <eric@zinnikas.org>
2013-11-30 03:46:13 -0500

    Initial commit for #4

---
+++ lib/tracker.rb
@@ -0,0 +1,9 @@
+#Handle communication w/tracker (i.e. HTTP(S) GET requests)
+#Need to implement proper urlencoding (See: https://wiki.theory.org/BitTorrentSpecification#Tracker_HTTP.2FHTTPS_Protocol )
+
+module Torrent
+
+class Tracker
+end
+
+end
