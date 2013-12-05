1f20032fabc18567eaee5675b05a2b9012c71455
cs417010@csic.umd.edu <cs417010@nauseated.csic.umd.edu>
2013-12-04 22:41:11 -0500

    Wrote handshake and initialize for peer class.

--- lib/peer.rb
+++ lib/peer.rb
@@ -1,5 +1,25 @@
-module Torrent
-	#test
+module Torrent	
 	class Peer
+		@local_peer_id = nil
+		@recipient_peer_id = nil
+		@info_hash = nil
+		
+		def initialize(tracker)
+			@local_peer_id = tracker.getPeerId
+			@info_hash = tracker.getInfoHash
+		end
+		
+		def handshake(ip, port)
+			pstrlen = "19"
+			pstr = "BitTorrent protocol"
+			reserved = "00000000"
+			
+			socket = TCPSocket.new(ip, port)
+			socket.write(pstrlen + pstr + reserved + info_hash + local_peer_id)
+			while line = socket.gets
+			  puts line
+			end
+			socket.close
+		end
 	end
 end
