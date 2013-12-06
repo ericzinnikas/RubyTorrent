01ea10999486f315c20bbe1b2b100f2a66735453
Ben Gmurczyk <b.gmurczyk@gmail.com>
2013-12-05 21:28:42 -0500

    Added higher-level client class and began changing peers class to use array of ips and ports.

--- lib/peer.rb
+++ lib/peer.rb
@@ -1,25 +1,32 @@
+# Handle incoming and outgoing communication with other peers
+
 module Torrent	
-	class Peer
-		@local_peer_id = nil
-		@recipient_peer_id = nil
-		@info_hash = nil
-		
-		def initialize(tracker)
-			@local_peer_id = tracker.getPeerId
-			@info_hash = tracker.getInfoHash
-		end
-		
-		def handshake(ip, port)
-			pstrlen = "19"
-			pstr = "BitTorrent protocol"
-			reserved = "00000000"
-			
-			socket = TCPSocket.new(ip, port)
-			socket.write(pstrlen + pstr + reserved + @info_hash + @local_peer_id)
-			while line = socket.gets
-			  puts line
-			end
-			socket.close
-		end
-	end
+
+class Peer
+  @peers = nil
+  
+  @local_peer_id = nil
+  @info_hash = nil
+  
+  def initialize(tracker)
+    @peers = tracker.getPeers
+    @local_peer_id = tracker.getPeerId
+    @info_hash = tracker.getInfoHash
+  end
+  
+  # 'peer' argument is the index of the peer in the peers array.
+  def handshake(peer)
+    pstrlen = "19"
+    pstr = "BitTorrent protocol"
+    reserved = "00000000"
+    
+    socket = TCPSocket.new(peers[peer][0], peers[peer][1])
+    socket.write(pstrlen + pstr + reserved + @info_hash + @local_peer_id)
+    while line = socket.gets
+      puts line
+    end
+    socket.close
+  end
+end
+
 end
