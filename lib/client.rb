c901e5dcdb0278cc53da2ebae40719c9f1cea0b5
Ben Gmurczyk <b.gmurczyk@gmail.com>
2013-12-05 21:31:49 -0500

    Added client class from last commit.

---
+++ lib/client.rb
@@ -0,0 +1,33 @@
+# Handle the higher-level logic of downloading and seeding torrent files.
+
+module Torrent
+
+class Client
+  @torrent_file = nil
+
+  def initialize(file) 
+    require_relative "bencode"
+    require_relative "metainfo"
+    require_relative "peer"
+    require_relative "tracker"
+    
+    @torrent_file = File.open(file, "r")
+  end
+  
+  def runClient
+    metainfo = Metainfo.new(@torrent_file)
+    tracker = Tracker.new(metainfo)
+    tracker.sendRequest("started")
+    peer = Peers.new(tracker)
+    peer.handshake(0)
+  end
+end
+
+unless ARGV.length == 1
+  abort("Invalid number of arguments. Usage: ruby client.rb [file]")
+end
+
+client = Client.new(ARGV[0])
+client.runClient
+
+end
