1d223e953732539b4399ad47a66700a4d7a08c38
Eric Zinnikas <eric@zinnikas.org>
2013-12-16 23:32:11 -0500

    test

--- lib/fileio.rb
+++ lib/fileio.rb
@@ -66,8 +66,8 @@ class FileIO
       @files << [File.open(prefix_dir + info["name"], File::RDWR | File::CREAT), info["length"]]
       if @files.last[0].size < @files.last[1]
         @files.last[0].seek(@files.last[0].size, IO::SEEK_SET)
-        (0..(@files.last[1] - @files.last[0].size)).each {
-          @files.last[0].write("\0")
+        (0..((@files.last[1] - @files.last[0].size)/1024)).each {
+          @files.last[0].write("\0" * 1024)
         }
       end
     end
